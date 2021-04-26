eab0a72050c5435794b765951aa19847b119eeb9
Bishwajit Jha <ajitjha393@gmail.com>
2021-04-26 11:06:20 +0530

    Event Item component

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,7 +1,36 @@
-function EventItem(props) {
-	// const { events } = props
+import Link from 'next/link'
 
-	return <li></li>
+function EventItem(props) {
+	const { title, image, date, location, id } = props
+
+	const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+		day: 'numeric',
+		month: 'long',
+		year: 'numeric',
+	})
+
+	const formattedAddress = location.replace(', ', '\n')
+
+	return (
+		<li>
+			<img src={'/' + image} alt="" />
+			<div>
+				<div>
+					<h2>{title}</h2>
+					<div>
+						<time>{humanReadableDate}</time>
+					</div>
+					<div>
+						<address>{formattedAddress}</address>
+					</div>
+				</div>
+
+				<div>
+					<Link href={`/events/${id}`}>Explore Event</Link>
+				</div>
+			</div>
+		</li>
+	)
 }
 
 export default EventItem
