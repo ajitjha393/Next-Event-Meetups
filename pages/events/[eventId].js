a681886a144e0c60e0a422d3482425c1bcaf6694
Bishwajit Jha <ajitjha393@gmail.com>
2021-04-27 11:30:13 +0530

    Fecthing EventId from Dynamic Url

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,17 @@
+import { useRouter } from 'next/router'
+import { getEventById } from '../../dummy-data.js'
+
 function EventDetailPage() {
+	const { query } = useRouter()
+
+	const eventId = query.eventId
+
+	const event = getEventById(eventId)
+
+	if (!event) {
+		return <p>No Event Found</p>
+	}
+
 	return (
 		<div>
 			<h1>Selected Event Detail Page!</h1>
