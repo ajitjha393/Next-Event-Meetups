913b54452d4a545a3bc28eb26e2e1982d57b584d
Bishwajit Jha <ajitjha393@gmail.com>
2021-05-01 20:21:53 +0530

    Added error alert finished wih proj

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -5,6 +5,8 @@ import EventSummary from '../../components/event-detail/event-summary'
 import EventContent from '../../components/event-detail/event-content'
 import EventLogistics from '../../components/event-detail/event-logistics'
 
+import ErrorAlert from '../../components/ui/error-alert'
+
 function EventDetailPage() {
 	const { query } = useRouter()
 
@@ -13,7 +15,11 @@ function EventDetailPage() {
 	const event = getEventById(eventId)
 
 	if (!event) {
-		return <p>No Event Found</p>
+		return (
+			<ErrorAlert>
+				<p>No Event Found</p>
+			</ErrorAlert>
+		)
 	}
 
 	const { title, description, location, date, image } = event
