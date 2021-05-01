405670407dab3fd4993627fa63dbac2bfb76c1d2
Bishwajit Jha <ajitjha393@gmail.com>
2021-05-01 18:53:37 +0530

    Added styling for events-search page

--- components/events/events-search.js
+++ components/events/events-search.js
@@ -1,12 +1,13 @@
 import Button from '../ui/button'
+import classes from './events-search.module.css'
 
 function EventsSearch(props) {
 	const formSubmitHandler = () => {}
 
 	return (
-		<form>
-			<div>
-				<div>
+		<form className={classes.form}>
+			<div className={classes.controls}>
+				<div className={classes.control}>
 					<label htmlFor="year">Year</label>
 					<select name="year" id="year">
 						<option value="2021">2021</option>
@@ -14,7 +15,7 @@ function EventsSearch(props) {
 					</select>
 				</div>
 
-				<div>
+				<div className={classes.control}>
 					<label htmlFor="month">Month</label>
 					<select name="month" id="month">
 						<option value="1">January</option>
