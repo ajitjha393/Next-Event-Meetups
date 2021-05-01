4c51bd85cfe5317054346c7fc4fa85e2a2e71dd6
Bishwajit Jha <ajitjha393@gmail.com>
2021-05-01 20:20:02 +0530

    Wrapped Error ALert

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -3,6 +3,8 @@ import { getFilteredEvents } from '../../dummy-data'
 import EventList from '../../components/events/event-list'
 import { Fragment } from 'react'
 import ResultsTitle from '../../components/events/results-title'
+import Button from '../../components/ui/button'
+import ErrorAlert from '../../components/ui/error-alert'
 
 function FilteredEventsPage() {
 	const router = useRouter()
@@ -23,7 +25,16 @@ function FilteredEventsPage() {
 		numMonth < 1 ||
 		numMonth > 12
 	) {
-		return <p>Invalid Filter. Please adjust your values</p>
+		return (
+			<Fragment>
+				<ErrorAlert>
+					<p>Invalid Filter. Please adjust your values</p>
+				</ErrorAlert>
+				<div className="center">
+					<Button link="/events">Show All events</Button>
+				</div>
+			</Fragment>
+		)
 	}
 
 	const filteredEvents = getFilteredEvents({
@@ -32,7 +43,16 @@ function FilteredEventsPage() {
 	})
 
 	if (!filteredEvents || filteredEvents.length === 0) {
-		return <p>No events found for the chosen filter</p>
+		return (
+			<Fragment>
+				<ErrorAlert>
+					<p>No events found for the chosen filter</p>
+				</ErrorAlert>
+				<div className="center">
+					<Button link="/events">Show All events</Button>
+				</div>
+			</Fragment>
+		)
 	}
 
 	const date = new Date(numYear, numMonth - 1)
