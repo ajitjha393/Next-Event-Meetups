96859a47733c9d364f8c2df5068d4e2d16862deb
Bishwajit Jha <ajitjha393@gmail.com>
2021-05-01 19:07:52 +0530

    Added Events Search Section

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
+import { Fragment } from 'react'
 import { getFeaturedEvents } from '../dummy-data'
 import EventList from '../components/events/event-list'
+import EventsSearch from '../components/events/events-search'
 
 function HomePage() {
 	const featuredEvents = getFeaturedEvents()
 	return (
-		<div>
+		<Fragment>
+			<EventsSearch />
 			<EventList events={featuredEvents} />
-		</div>
+		</Fragment>
 	)
 }
 
