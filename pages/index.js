909e90d2a1c4481f0edbc9b1abb6a2950db5e7b0
Bishwajit Jha <ajitjha393@gmail.com>
2021-05-10 18:59:36 +0530

    Added events fetching Statically on backend

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,10 @@ import { useRouter } from 'next/router'
 import { getFeaturedEvents } from '../dummy-data'
 import EventList from '../components/events/event-list'
 import EventsSearch from '../components/events/events-search'
+import axios from 'axios'
 
-function HomePage() {
+function HomePage({ featuredEvents }) {
 	const router = useRouter()
-	const featuredEvents = getFeaturedEvents()
 
 	const findEventsHandler = (year, month) => {
 		const fullPath = `/events/${year}/${month}`
@@ -20,6 +20,26 @@ function HomePage() {
 			<EventList events={featuredEvents} />
 		</Fragment>
 	)
+}
+
+export async function getStaticProps() {
+	const allEvents = []
+	const { data: resData } = await axios.get(
+		'https://react-meetup-events-default-rtdb.firebaseio.com/events.json'
+	)
+
+	for (const keys in resData) {
+		allEvents.push({
+			id: keys,
+			...resData[keys],
+		})
+	}
+
+	return {
+		props: {
+			featuredEvents: allEvents.filter(event => event.isFeatured),
+		},
+	}
 }
 
 export default HomePage
